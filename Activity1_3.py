5566900b9ba9e9ba691473f08548a0d2c126e4e6
muneba-2025 <tania.anis2023@gmail.com>
2025-10-03 20:43:03 +0200

    p3

---
+++ Activity1_3.py
@@ -0,0 +1,6 @@
+a=5
+print("type of a: ",type(a))
+n=45.6
+print("type of n:",type(n))
+c="coding"
+print("type of c:",type(c))
---
+++ Activity2_3.py
@@ -0,0 +1,22 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("name: ",name)
+print("data type of name is",type(name))
+print("age: ",age)
+print("data type of age is",type(age))
+print("is_student:",is_student)
+print("data type of is_student is",type(is_student))
+print("weight:",weight)
+print("data name of weight is",type(weight))
+
+print("\n after typecasting...")
+age=str(age)
+print("age: ",age)
+print("data type of age is",type(age))
+weight=int(weight)
+print("weight:",weight)
+print("data name of weight is",type(weight))
+
+
