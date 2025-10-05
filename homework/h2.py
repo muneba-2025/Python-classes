0d4b01871fe87fc52c3734c9c1ae5ea84bdbf091
muneba-2025 <tania.anis2023@gmail.com>
2025-10-05 16:10:30 +0200

    p5

---
+++ homework/h2.py
@@ -0,0 +1,8 @@
+#Taking birthday as input from user
+date=int(input("please enter birth date: "))
+#Taking name as input from user
+name=int(input("please enter name: "))
+#Only number for date variable
+
+print(date)
+print(name)
