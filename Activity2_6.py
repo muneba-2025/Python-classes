38df550fe758eb9eeab7c52944ad0d93d145edc0
muneba-2025 <tania.anis2023@gmail.com>
2025-10-11 20:39:34 +0200

    p8

---
+++ Activity2_6.py
@@ -0,0 +1,17 @@
+h=10
+n=12
+s=12
+print(h != n)
+print(h != s)
+
+b="python"
+v="coding"
+if b != v:
+    print("b and v are different")
+b=4
+l=5
+if(b == 1)!=(l==5):
+    print("hello")
+f=int(input("enter a number: "))
+if f%2 !=0:
+    print("the number is not an even number")
--- homework/h5.py
+++ homework/h5.py
@@ -1,1 +1,6 @@
-num=int(input("please enter a number: "))
+num=float(input("please enter a number: "))
+if num>0:
+    sqrt=num**0.5
+    print("square root of num is ",sqrt)
+else:
+    print("square root of num does not exist")
