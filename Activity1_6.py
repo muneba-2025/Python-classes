546fef25a1a09bd623a64ddcb25c2c0ba544c1a8
muneba-2025 <tania.anis2023@gmail.com>
2025-10-11 20:17:12 +0200

    p7

---
+++ Activity1_6.py
@@ -0,0 +1,15 @@
+g=10
+k=12
+m=0
+if g and k and m:
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast 1 number has boolean value as false")
+
+g=10
+k=-10
+m=0
+if g>0 or k>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
