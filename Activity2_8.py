8e01dda545b73276313b0f72dd1e2f08f2db2727
muneba-2025 <tania.anis2023@gmail.com>
2025-10-25 20:22:59 +0200

    p13

---
+++ Activity2_8.py
@@ -0,0 +1,8 @@
+string=input("Please enter your own string: ")
+string2=("")
+for i in string:
+    string2=i+string2
+print("\nthe original string: ",string)
+print("The reverse string: ",string2)
+
+
