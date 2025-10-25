575c894b92631fc5328139cf2e0075ac0365da1c
muneba-2025 <tania.anis2023@gmail.com>
2025-10-25 21:13:09 +0200

    p14

---
+++ homework/h8.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age: "))
+if (age>10):
+    print("You can come into class")
+else:
+    print("You cannot come into this class")
+
+if (age>20):
+    print("You cannot come into this class")
