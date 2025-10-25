458539bed4d6537d755cacfb89338cde84c99e69
muneba-2025 <tania.anis2023@gmail.com>
2025-10-25 21:21:28 +0200

    p15

---
+++ homework/h9.py
@@ -0,0 +1,3 @@
+num=int(input("Please enter a number: "))
+print("This number has a power. Figure it out by multiplying the number by itself a couple of times!")
+print("Thank you for bieng here. Hope you find your answer.")
