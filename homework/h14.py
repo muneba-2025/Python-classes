fc8ed4cd6e79c81ef39b256bb9ccfcca566628d1
muneba-2025 <tania.anis2023@gmail.com>
2025-12-06 21:38:20 +0200

    p20

---
+++ homework/h14.py
@@ -0,0 +1,11 @@
+
+print("Conditions for shutting down laptop")
+print("All tabs cleared")
+print("Charge accurate")
+print("All work done")
+int(input("Please enter yes or no"))
+if yes: print("Shutdown")
+else:
+    print("Check again")
+
+        
