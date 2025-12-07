a05032efe38e495433d9402b4294681bf7dab9f6
muneba-2025 <tania.anis2023@gmail.com>
2025-12-07 20:11:00 +0200

    p20

---
+++ homework/h15.py
@@ -0,0 +1,4 @@
+print("How to calculate the curcumference of a circle: ")
+print("Multiply the radius by 2 to get the diameter.")
+print("Multiply that result by 3.14.")
+print("That is it! You have found the curcumference of the circle!!")
