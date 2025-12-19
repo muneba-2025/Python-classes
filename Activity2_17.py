94d4c02c85f4e9b03eccc6ee0bf46970ebc482f8
muneba-2025 <tania.anis2023@gmail.com>
2025-12-19 20:37:07 +0200

    p23

---
+++ Activity2_17.py
@@ -0,0 +1,19 @@
+import random
+
+options=["Rock","Paper","Scissors"]
+user_choice= input("Choose Rock, Paper or Scissors: ")
+computer_choice=random.choice(options)
+
+print("You chose:",user_choice)
+print("Computor chose:",computer_choice)
+
+if user_choice==computer_choice:
+    print("It's a tie!")
+elif user_choice=="Rock" and computer_choice=="Scissors":
+    print("Rock smashes Scissors, You win!")
+elif user_choice=="Paper" and computer_choice=="Rock":
+    print("Paper covers Rock, You win!")
+elif user_choice=="Scissors" and computer_choice=="Paper":
+    print("Scissors cuts Paper, You win!")
+else:
+    print("You lose!")
