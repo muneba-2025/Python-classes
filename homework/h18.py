411fb9635d7d8102c03cdb4412e3b2ff4ab00ddb
muneba-2025 <tania.anis2023@gmail.com>
2025-12-20 21:58:43 +0200

    p24

---
+++ homework/h18.py
@@ -0,0 +1,12 @@
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print(math.sin(90))
+print(math.cos(90))
+print(math.tan(90))
+
+print(math.sin(180))
+print(math.cos(180))
+print(math.tan(180))
