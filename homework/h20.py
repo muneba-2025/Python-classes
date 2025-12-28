bb92b6b8739c0944a9f406cf89cee031e3f12ac7
muneba-2025 <tania.anis2023@gmail.com>
2025-12-28 20:22:52 +0200

    p30

---
+++ homework/h20.py
@@ -0,0 +1,9 @@
+numbers=[2,9,16,25,36]
+square =numbers**2
+print(square)
+def check_odd_even(number):
+  if number % 2 == 0:
+    return f"{number} is Even"
+  else:
+    return f"{number} is Odd"
+
