8dde040b591460937c20d64a853a5e8039657734
muneba-2025 <tania.anis2023@gmail.com>
2026-01-05 16:45:41 +0200

    p26

---
+++ homework/h21.py
@@ -0,0 +1,4 @@
+test_dict={'Codingal':3,'is':2,'best':2,'for':2,'Coding':1}
+print(test_dict)
+int(input("Enter the value in the Dictionary you want to find the frequency of: "))
+print("Check the dictionary")
