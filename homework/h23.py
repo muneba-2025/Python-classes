07b62f3d6716205f58f9f1bc9adb9298e2c3530c
muneba-2025 <tania.anis2023@gmail.com>
2026-01-16 20:30:10 +0200

    p30

---
+++ homework/h23.py
@@ -0,0 +1,10 @@
+set1={'blue','green'}
+set2={'blue','yellow'}
+print(set1,set2)
+set1.symmetric_difference(set2)
+
+
+set1={1,2,3,4,5}
+set2={1,5,6,7,8,9}
+print(set1,set2)
+set1.symmetric_difference(set2)
