942b23b238218e0c26b45ab8663821b7e9703c0d
muneba-2025 <tania.anis2023@gmail.com>
2026-01-24 20:39:44 +0200

    p36

---
+++ Activity3_26.py
@@ -0,0 +1,17 @@
+#Create a class
+class pair_elements:
+
+    def twoSum(self,nums,target):
+        #Create an empty dictionary
+        lookup={}
+
+
+        #Iterate through the tuple
+        for i,num in enumerate(nums):
+           if target - num in lookup:
+               return (lookup[target-num],i)
+           lookup[num]= i
+
+#Take input from user
+value = int(input("Enter sim for which you want to make this search: "))
+print("index1=%d, index2=%d"% pair_elements().twoSum((10,20,30,40,50,60,70),value))
